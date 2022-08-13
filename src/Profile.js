f7e3d96711242671c9e9ada6e8d16fa3b4cb40e2
nikhil borge <nikhilb3439.nb@gmail.com>
2022-08-13 16:18:34 +0530

    toogle using class based components

--- src/Profile.js
+++ src/Profile.js
@@ -7,6 +7,7 @@ export default class Profile extends React.Component {
       name: "nikhil",
       age: 20,
       count: 0,
+      show:true,
     };
   }
   updateState() {
@@ -15,15 +16,23 @@ export default class Profile extends React.Component {
       count: this.state.count + 1,
     });
   }
+ 
   render() {
+   
+
     return (
       <div>
+       {
+        this.state.show ?
+        <h1> Hide and show </h1>
+        :null
+      }
         <h1>hello {this.state.name}</h1>
         <h3>count {this.state.count}</h3>
         <h2>age is {this.state.age}</h2>
         <button
           onClick={() => {
-            this.updateState();
+            this.setState({show: !this.state.show});
           }}
         >
           change state
